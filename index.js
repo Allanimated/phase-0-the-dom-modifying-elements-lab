031b783a70ad3e2c8483b42f1da481815a68a208
Allan <allcreativewrites@gmail.com>
2023-09-16 13:50:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+//remove <main> node from the DOM
+const removeMain = document.querySelector("#main");
+removeMain.remove();
+//Has a newHeader variable that points to an <h1> node;
+const newHeader = document.createElement("h1");
+//the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.id = "victory";
+// the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
